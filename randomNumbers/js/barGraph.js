1f47380535e0c26a9c7e4ce2cf46589c8e6f3550
NavyaSriB <basava@thoughtworks.com>
2016-10-29 11:47:10 +0530

    refactor code

--- randomNumbers/js/barGraph.js
+++ randomNumbers/js/barGraph.js
@@ -2,8 +2,8 @@ var data = [10, 55, 23, 14, 12, 13, 54, 20];
 var colors = ["#1C86EE", "#60AFFE", "#499DF5", "#C6E2FF", "#7AA9DD", "#B9D3EE", "#1D7CF2", "#7EB6FF", "#BCD2EE", "#3A66A7"];
 
 var load = function() {
-    barsDiv = d3.select('.container').append('div').classed("bars", true);
-    var bars = barsDiv.selectAll("div")
+    var barsDiv = d3.select('.container').append('div').classed("bars", true);
+    var bars = barsDiv.select("div")
         .data(data);
 
     bars.enter()
@@ -12,8 +12,12 @@ var load = function() {
         .style("height", "30px");
 
     var bar = d3.selectAll(".bar");
+    updateBar(bar);
+};
+
+var updateBar = function(bar) {
     bar.style("width", "0px")
-        .transition().duration(500)
+        .transition().duration(2000)
         .style("width", function (d) {return d*6+"px"})
         .text(function (d) { return d;})
         .style("background", function(d, i){return colors[d%10]});
@@ -22,10 +26,8 @@ var load = function() {
 var updateBars = function() {
     console.log(data);
     var bar = d3.selectAll(".bar").data(data);
-    bar.style("width", "0px")
-        .style("width", function (d) {return d*6+"px"})
-        .text(function (d) { return d;})
-        .style("background", function(d, i){return colors[d%10]});
+    updateBar(bar);
+    bar.exit().remove();
 };
 
 function randomNumber() {
@@ -35,7 +37,7 @@ setInterval(function () {
     var num = data.shift();
     data.push(randomNumber());
     updateBars();
-},1000);
+},2000);
 
 window.onload = function () {
     load();
