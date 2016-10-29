fa7ce00b9a0ed2875bebc68b72d93da410850afb
NavyaSriB <basava@thoughtworks.com>
2016-10-29 12:08:59 +0530

    assign colours using d3.rgb

--- randomNumbers/js/barGraph.js
+++ randomNumbers/js/barGraph.js
@@ -1,5 +1,4 @@
 var data = [10, 55, 23, 14, 12, 13, 54, 20];
-var colors = ["#1C86EE", "#60AFFE", "#499DF5", "#C6E2FF", "#7AA9DD", "#B9D3EE", "#1D7CF2", "#7EB6FF", "#BCD2EE", "#3A66A7"];
 
 var load = function() {
     var barsDiv = d3.select('.container').append('div').classed("bars", true);
@@ -17,10 +16,9 @@ var load = function() {
 
 var updateBar = function(bar) {
     bar.style("width", "0px")
-        .transition().duration(2000)
         .style("width", function (d) {return d*6+"px"})
         .text(function (d) { return d;})
-        .style("background", function(d, i){return colors[d%10]});
+        .style("background", function(d, i){return d3.rgb(95,143,d+155)});
 };
 
 var updateBars = function() {
@@ -37,7 +35,7 @@ setInterval(function () {
     var num = data.shift();
     data.push(randomNumber());
     updateBars();
-},2000);
+},1000);
 
 window.onload = function () {
     load();
