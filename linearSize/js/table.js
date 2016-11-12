d79efa4a10ff6056037f07c2836bc98bfa1748c8
NavyaSriB <basava@thoughtworks.com>
2016-11-12 11:20:30 +0530

    refactor code

--- linearSize/js/table.js
+++ linearSize/js/table.js
@@ -1,27 +1,25 @@
-const LIMIT = 10;
 var titles = ['Title', 'n', 'n square', 'log(n)', 'log(n) rounded'];
-var generateValues = function(limit){
-    return [...new Array(limit)].map((d,i)=>i+1);
-};
-
-var header = ['Title', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 var numberScale = d3.scaleLinear().domain([1, 10]).range([1, 10]);
 var powerScale = d3.scalePow().exponent(2);
 var logScale = function (d) {
     return d3.scaleLog()(d).toFixed(4);
 };
 var roundedLogScale = d3.scaleLog().rangeRound([0, 1]);
-
 var tableContent;
 
-var loadTable = function () {
+var getNumbersWithTitle = function (title) {
+    var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    numbers.unshift(title);
+    return numbers;
+};
+
+var loadTable = function (headerData) {
     var table = d3.select('.container').append('table');
     var tableHead = table.append('thead');
     tableContent = table.append('tbody');
-
     tableHead.append('tr')
         .selectAll('th')
-        .data(header).enter()
+        .data(headerData).enter()
         .append('th')
         .text(function (d) {
             return d;
@@ -30,9 +28,10 @@ var loadTable = function () {
 
 var generateRows = function (data, scale) {
     var rows = tableContent.append('tr');
-
     rows.selectAll('td')
-        .data(function () { return data.map((ele)=>isNaN(scale(ele)) ? ele : scale(ele)) })
+        .data(function () {
+            return data.map((ele) => isNaN(scale(ele)) ? ele : scale(ele))
+        })
         .enter()
         .append('td')
         .text(function (d) {
@@ -41,21 +40,9 @@ var generateRows = function (data, scale) {
 };
 
 window.onload = function () {
-    loadTable();
-
-    var values = generateValues(LIMIT);
-    values.unshift(titles[1]);
-    generateRows(values, numberScale);
-
-    values.shift();
-    values.unshift(titles[2]);
-    generateRows(values, powerScale);
-
-    values.shift();
-    values.unshift(titles[3]);
-    generateRows(values, logScale);
-
-    values.shift();
-    values.unshift(titles[4]);
-    generateRows(values, roundedLogScale);
+    loadTable(getNumbersWithTitle(titles[0]));
+    generateRows(getNumbersWithTitle(titles[1]), numberScale);
+    generateRows(getNumbersWithTitle(titles[2]), powerScale);
+    generateRows(getNumbersWithTitle(titles[3]), logScale);
+    generateRows(getNumbersWithTitle(titles[4]), roundedLogScale);
 };
