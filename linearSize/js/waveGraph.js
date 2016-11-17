688a2f38b6d43c9c762d51b74547e34a28bdae17
NavyaSriB <basava@thoughtworks.com>
2016-11-17 17:02:58 +0530

    exercise10 linear sine wave [Shruti/Navya]

--- linearSize/js/waveGraph.js
+++ linearSize/js/waveGraph.js
@@ -2,12 +2,7 @@ const WIDTH = 550;
 const HEIGHT = 550;
 const MARGIN = 50;
 
-var linePoints = [[0, 5], [1, 9], [2, 7], [3, 5], [4, 3], [6, 4], [7, 2], [8, 3], [9, 2]];
-
-var normalizeCoordinate = function (coordinate) {
-    return coordinate / 10;
-};
-
+var xValues = [0,1,2,3,4,5,6,7,8,9];
 var loadGraph = function (interpolationMethod, typeName) {
     var container = d3.select('body')
         .append('div')
@@ -24,7 +19,7 @@ var loadGraph = function (interpolationMethod, typeName) {
         .attr('font-size', 'larger');
 
     var xScale = d3.scaleLinear()
-        .domain([0, 1])
+        .domain([0, 10])
         .range([0, WIDTH - MARGIN]);
     var yScale = d3.scaleLinear()
         .domain([0, 1])
@@ -43,10 +38,10 @@ var loadGraph = function (interpolationMethod, typeName) {
         .call(yAxis);
     var line = d3.line()
         .x(function (d) {
-            return xScale(normalizeCoordinate(d[0]))
+            return xScale(d)
         })
         .y(function (d) {
-            return yScale(normalizeCoordinate(d[1]))
+            return yScale((Math.sin(3*d)+1)/2)
         })
         .curve(interpolationMethod);
 
@@ -55,54 +50,22 @@ var loadGraph = function (interpolationMethod, typeName) {
         .classed('line', true);
 
     pathEle.append('path')
-        .attr('d', line(linePoints))
-        .attr('stroke', 'darkred');
-
-    var sineLine = d3.line()
-        .x(function (d) {
-            return xScale(normalizeCoordinate(d[0]))
-        })
-        .y(function (d) {
-            return yScale(normalizeCoordinate(Math.sin(d[0])) + 0.5)
-        })
-        .curve(interpolationMethod);
-
-    pathEle.append('path')
-        .attr('d', sineLine(linePoints))
-        .attr('stroke', 'darkblue');
+        .attr('d', line(xValues));
 
     var circles = pathEle.selectAll('circle')
-        .data(linePoints);
+        .data(xValues);
 
     circles.enter()
         .append('circle')
         .attr('cx', function (d) {
-            return xScale(normalizeCoordinate(d[0]))
+            return xScale(d)
         })
         .attr('cy', function (d) {
-            return yScale(normalizeCoordinate(Math.sin(d[0])) + 0.5)
+            return yScale((Math.sin(3*d)+1)/2)
         })
         .attr('r', 5);
-
-    circles.enter()
-        .append('circle')
-        .attr('cx', function (d) {
-            return xScale(normalizeCoordinate(d[0]))
-        })
-        .attr('cy', function (d) {
-            return yScale(normalizeCoordinate(d[1]))
-        })
-        .attr('r', 5);
-
 };
 
 window.onload = function () {
     loadGraph(d3.curveLinear, 'curve linear');
-    loadGraph(d3.curveLinearClosed, 'curve linear closed');
-    loadGraph(d3.curveStepAfter, 'curve step after');
-    loadGraph(d3.curveBasis, 'curve basics');
-    loadGraph(d3.curveBundle, 'curve bundle');
-    loadGraph(d3.curveCardinalClosed, 'curve cardinal closed');
-    loadGraph(d3.curveCardinal, 'curve cardinal');
-    loadGraph(d3.curveCatmullRom.alpha(0.5), 'curve catmul rom');
 };
