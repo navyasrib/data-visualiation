0ff2aa6c4a3ea51f92679341f532e65512e30741
NavyaSriB <basava@thoughtworks.com>
2016-11-19 14:11:26 +0530

    user can choose the radius of pie & donut charts

--- linearSize/js/pieChart.js
+++ linearSize/js/pieChart.js
@@ -2,10 +2,36 @@ const WIDTH = 600;
 const HEIGHT = 600;
 var values = [1, 1, 2, 2, 1, 2, 1];
 var colors = d3.scaleOrdinal(d3.schemeCategory20);
-
 var container = d3.select('body').append('container');
 
-var loadChart = function(outerRadius, innerRadius,angleRatio) {
+var data = [
+    {name:'PieChart', innerRadius:250, outerRadius:0, angleRatio:1},
+    {name:'Half PieChart', innerRadius:250, outerRadius:0, angleRatio:2},
+    {name:'DonutChart', innerRadius:250, outerRadius:125, angleRatio:1},
+    {name:'Half DonutChart', innerRadius:250, outerRadius:125, angleRatio:2}
+];
+
+var createButton = function(dropDown, specifications) {
+    var chart = dropDown.append('button')
+        .text(specifications.name);
+    chart.on('click', function(){
+        var iradius = document.getElementById('iradius').value;
+        var oradius = document.getElementById('oradius').value;
+        specifications.innerRadius = iradius? iradius : specifications.innerRadius;
+        specifications.outerRadius = oradius? oradius : specifications.outerRadius;
+        loadChart(specifications);
+    });
+};
+
+var module = function() {
+    var dropDown = container.append('div').classed('dropDown', true);
+    data.forEach(function(chart){
+       createButton(dropDown, chart);
+    });
+};
+
+var loadChart = function(circleSpec) {
+    container.select('svg').remove();
     var svg = container.append('svg');
 
     var g = svg.attr('width', WIDTH)
@@ -17,10 +43,10 @@ var loadChart = function(outerRadius, innerRadius,angleRatio) {
         .sort(null)
         .value(function(d) { return d; });
     var arc = d3.arc()
-        .outerRadius(outerRadius)
-        .innerRadius(innerRadius)
-        .startAngle(function(d) {return d.startAngle/angleRatio})
-        .endAngle(function(d) {return d.endAngle/angleRatio});
+        .outerRadius(circleSpec.outerRadius)
+        .innerRadius(circleSpec.innerRadius)
+        .startAngle(function(d) {return d.startAngle/circleSpec.angleRatio})
+        .endAngle(function(d) {return d.endAngle/circleSpec.angleRatio});
 
     g.selectAll('path')
         .data(pie(values))
@@ -31,8 +57,5 @@ var loadChart = function(outerRadius, innerRadius,angleRatio) {
 };
 
 window.onload = function() {
-    loadChart(250, 0, 1);
-    loadChart(250, 125, 1);
-    loadChart(250, 0, 2);
-    loadChart(250, 125, 2);
+    module();
 };
